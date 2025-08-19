8c593440fbd61faf8a9015a17d33704bc8274519
annetnyabuto <annetgnyabuto@gmail.com>
2025-08-19 21:41:25 +0300

    Work on author class

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -6,13 +6,32 @@ class Article:
         
 class Author:
     def __init__(self, name):
-        self.name = name
-
+        if not isinstance(name, str):
+            raise TypeError("Name must be of type string")
+        if len(name) == 0:
+            raise ValueError("Name must be longer than 0 characters")
+        self._name = name 
+    
+    @property
+    def name(self):
+        return self._name # name can not change after author is instantiated
+    
     def articles(self):
         pass
-
-    def magazines(self):
-        pass
+    class Magazine:
+        def __init__(self, name, category):
+            self.name = name
+            self.category = category
+        @property
+        def name(self):
+            self._name
+        @name.setter
+        def name(self, value):
+            if not isinstance(value, str):
+                raise TypeError("Name must be of type str")
+            if not(2 <= len(value) <= 16):
+                raise ValueError("Name must be between 2 and 16 characters")
+            self._name = value  
 
     def add_article(self, magazine, title):
         pass
