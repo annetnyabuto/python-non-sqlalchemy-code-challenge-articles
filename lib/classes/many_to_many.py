53d480dc4f38f5d414d887512aba7429bef17d93
annetnyabuto <annetgnyabuto@gmail.com>
2025-08-19 22:22:47 +0300

    working on magazine class

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,9 +1,3 @@
-class Article:
-    def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
 class Author:
     def __init__(self, name):
         if not isinstance(name, str):
@@ -17,41 +11,47 @@ class Author:
         return self._name # name can not change after author is instantiated
     
     def articles(self):
-        pass
-    class Magazine:
-        def __init__(self, name, category):
-            self.name = name
-            self.category = category
-        @property
-        def name(self):
-            self._name
-        @name.setter
-        def name(self, value):
-            if not isinstance(value, str):
-                raise TypeError("Name must be of type str")
-            if not(2 <= len(value) <= 16):
-                raise ValueError("Name must be between 2 and 16 characters")
-            self._name = value  
-
+        return [article for article in Article.all if article.author == self]
+    
+    def magazines(self):
+        return list(set([article.magazine for article in self.articles()]))
+    
     def add_article(self, magazine, title):
-        pass
-
+        return Article(self, magazine, title)
+    
     def topic_areas(self):
-        pass
-
+        areas = [magazine.category for magazine in self.magazines()]
+        return list(set(areas)) if areas else None
+    
 class Magazine:
     def __init__(self, name, category):
+        self._name = None
+        self._category = None
         self.name = name
         self.category = category
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be of type str")
+        if not(2 <= len(value) <= 16):
+            raise ValueError("Name must be between 2 and 16 characters")
+        self._name = value 
+    
+    @property
+    def category(self):
+        return self._category
 
-    def articles(self):
-        pass
-
-    def contributors(self):
-        pass
-
-    def article_titles(self):
-        pass
-
-    def contributing_authors(self):
-        pass
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Category must be of type str")
+        if len(value) == 0:
+            raise ValueError("Category must be longer than 0 characters")
+        self._category = value
+    
+    
